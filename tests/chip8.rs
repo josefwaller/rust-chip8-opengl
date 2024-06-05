1efa6b07b7d4e36c044abe133ef5628467acd7f3
Josef Waller <josef@siriusapplications.com>
2024-06-04 17:42:45 -0700

    Refactor tests

--- tests/chip8.rs
+++ tests/chip8.rs
@@ -6,8 +6,11 @@ mod tests {
     // Build an instruction from 4 4bit values
     // Returns 0x[a][b][c][d]
     // All of a, b, c, d should be at most 4 bits long
-    fn build_inst(a: u16, b: u16, c: u16, d: u16) -> u16 {
-        return ((a & 0x0F) << 12) | ((b & 0x0F) << 8) | ((c & 0x0F) << 4) | (d & 0x0F) as u16;
+    fn build_inst(a: u8, b: u8, c: u8, d: u8) -> u16 {
+        return ((a as u16 & 0x0F) << 12)
+            | ((b as u16 & 0x0F) << 8)
+            | ((c as u16 & 0x0F) << 4)
+            | (d as u16 & 0x0F) as u16;
     }
     fn rand_byte(max: u8) -> u16 {
         return rand::thread_rng().gen_range(0..max) as u16;
@@ -16,205 +19,144 @@ mod tests {
     fn test_ld_vx_kk() {
         let mut emu = Chip8::new();
         let mut set_vals: [u8; 16] = [0; 16];
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            set_vals[i] = v as u8;
-            emu.step(build_inst(0x6, i as u16, v >> 4, v));
-            for j in (0..i).rev() {
+        for x in 0..15 {
+            let val_x = rand_byte(0xFF) as u8;
+            set_vals[x] = val_x as u8;
+            emu.step(build_inst(0x6, x as u8, val_x >> 4, val_x));
+            for j in (0..x).rev() {
                 assert_eq!(emu.get_register_value(j as u8), set_vals[j]);
             }
         }
     }
     #[test]
     fn test_ld_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            let target = rand_byte(0xF);
-            let set_inst = build_inst(0x6, i, v as u16 >> 4, v as u16);
-            emu.step(set_inst);
-            let mov_inst = build_inst(8, target as u16, i, 0);
-            emu.step(mov_inst);
-            assert_eq!(emu.get_register_value(target as u8), v as u8);
-        }
+        stress_test(|emu, x, y, _val_x, val_y| {
+            emu.step(build_inst(8, x, y, 0));
+            assert_eq!(emu.get_register_value(x), val_y);
+            assert_eq!(emu.get_register_value(y), val_y);
+        })
     }
     #[test]
     fn test_or_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF) as u16;
-            let v_target = rand_byte(0xFF) as u16;
-            let mut target = rand_byte(0xF) as u16;
-            if target >= i {
-                target += 1;
-            }
-            assert_ne!(i, target);
-            let set_inst = build_inst(0x6, i, v as u16 >> 4, v);
-            emu.step(set_inst);
-            let set_inst = build_inst(0x6, target, v_target >> 4, v_target);
-            emu.step(set_inst);
-            let or_inst = build_inst(8, target, i, 1);
-            emu.step(or_inst);
-        }
+        stress_test(|emu, x, y, val_x, val_y| {
+            emu.step(build_inst(8, x, y, 1));
+            assert_eq!(emu.get_register_value(x), val_x | val_y);
+        })
     }
     #[test]
     fn test_or_vx_vy_same_register() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            emu.step(build_inst(0x6, i, i >> 4, i));
-            emu.step(build_inst(8, i >> 4, i & 0x0F, 1));
-            assert_eq!(emu.get_register_value(i as u8), i as u8);
-        }
+        stress_test(|emu, x, _y, val_x, _val_y| {
+            emu.step(build_inst(8, x, x, 1));
+            assert_eq!(emu.get_register_value(x), val_x);
+        })
     }
     #[test]
     fn test_and_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            let mut target = rand_byte(0xE);
-            if target >= i {
-                target += 1;
-            }
-            let v_target = rand_byte(0xFF);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(0x6, target, v_target >> 4, v_target));
-            emu.step(build_inst(0x8, target, i, 2));
-            assert_eq!(emu.get_register_value(target as u8), (v_target & v) as u8);
-        }
+        stress_test(|emu, x, y, val_x, val_y| {
+            emu.step(build_inst(0x8, x, y, 2));
+            assert_eq!(emu.get_register_value(x), val_x & val_y);
+        })
     }
     #[test]
     fn test_and_vx_vy_same_register() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            emu.step(build_inst(0x6, i, i >> 4, i));
-            emu.step(build_inst(8, i, i & 0x0F, 2));
-            assert_eq!(emu.get_register_value(i as u8), i as u8);
-        }
+        stress_test(|emu, x, _y, val_x, _val_y| {
+            emu.step(build_inst(8, x, x, 2));
+            assert_eq!(emu.get_register_value(x), val_x);
+        })
     }
     #[test]
     fn test_xor_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            let mut target = rand_byte(0xE);
-            if target >= i {
-                target += 1;
-            }
-            let v_target = rand_byte(0xFF);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(0x6, target, v_target >> 4, v_target));
-            emu.step(build_inst(0x8, target, i, 3));
-            assert_eq!(emu.get_register_value(target as u8), (v_target ^ v) as u8);
-        }
+        stress_test(|emu, x, y, val_x, val_y| {
+            emu.step(build_inst(0x8, x, y, 3));
+            assert_eq!(emu.get_register_value(x), val_x ^ val_y);
+        })
     }
     #[test]
     fn test_xor_vx_vy_same_register() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            emu.step(build_inst(0x6, i, i >> 4, i));
-            emu.step(build_inst(8, i, i & 0x0F, 3));
-            assert_eq!(emu.get_register_value(i as u8), 0x00);
-        }
+        stress_test(|emu, x, _y, _val_x, _val_y| {
+            emu.step(build_inst(8, x, x, 3));
+            assert_eq!(emu.get_register_value(x), 0x00);
+        })
     }
     #[test]
     fn test_add_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            let mut target = rand_byte(0xE);
-            if target >= i {
-                target += 1;
-            }
-            let v_target = rand_byte(0xFF);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(0x6, target, v_target >> 4, v_target));
-            emu.step(build_inst(0x8, target, i, 4));
-            assert_eq!(
-                emu.get_register_value(target as u8),
-                (v_target + v) as u8 & 0xFF
-            );
+        stress_test(|emu, vx, vy, val_x, val_y| {
+            emu.step(build_inst(0x8, vx, vy, 4));
+            assert_eq!(emu.get_register_value(vx), val_x.wrapping_add(val_y));
             assert_eq!(
                 emu.get_register_value(0xF),
-                if v_target + v > 0xFF { 1 } else { 0 }
+                if (val_x as u16 + val_y as u16) > 0xFF {
+                    1
+                } else {
+                    0
+                }
             );
-        }
+        });
     }
     #[test]
     fn test_add_vx_vy_same_register() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            emu.step(build_inst(0x6, i, i >> 4, i));
-            emu.step(build_inst(8, i, i, 4));
-            assert_eq!(emu.get_register_value(i as u8), (2 * i) as u8 & 0xFF);
+        stress_test(|emu, vx, _vy, val_x, _val_y| {
+            emu.step(build_inst(8, vx, vx, 4));
+            assert_eq!(emu.get_register_value(vx), (val_x.wrapping_add(val_x)));
             assert_eq!(
                 emu.get_register_value(0xF),
-                if 2 * i > 0xFF { 1 } else { 0 }
+                if 2 * val_x as u16 > 0xFF { 1 } else { 0 }
             );
-        }
+        })
     }
     #[test]
     fn test_sub_vx_vy() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            // Set VF to verify it's cleared
-            emu.step(build_inst(0x6, 0xF, 0xF, 0xF));
-            let v_target = rand_byte(0xFE) + 1;
-            let mut target = rand_byte(0xE);
-            if target >= i {
-                target += 1;
-            }
-            let v = rand_byte(v_target as u8);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(0x6, target, v_target >> 4, v_target));
-            emu.step(build_inst(0x8, target, i, 5));
-            assert_eq!(
-                emu.get_register_value(target as u8),
-                (v_target - v) as u8 & 0xFF
-            );
-            assert_eq!(emu.get_register_value(0xF), 0);
-        }
+        stress_test(|emu, vx, vy, val_x, val_y| {
+            emu.step(build_inst(0x8, vx, vy, 5));
+            assert_eq!(emu.get_register_value(vx), val_x - val_y);
+        })
     }
     #[test]
     fn test_sub_vx_vy_same_register() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            emu.step(build_inst(0x6, i, i >> 4, i));
-            emu.step(build_inst(8, i, i, 5));
-            assert_eq!(emu.get_register_value(i as u8), 0);
+        stress_test(|emu, vx, _vy, _val_x, _val_y| {
+            emu.step(build_inst(8, vx, vx, 5));
+            assert_eq!(emu.get_register_value(vx), 0);
             assert_eq!(emu.get_register_value(0xF), 0);
-        }
+        });
     }
     #[test]
     fn test_sub_vx_vy_underflow() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            // Set VF to verify it's cleared
-            emu.step(build_inst(0x6, 0xF, 0x0, 0x0));
-            let v = rand_byte(0xFE) + 1;
-            let mut target = rand_byte(0xE);
-            if target >= i {
-                target += 1;
-            }
-            let v_target = rand_byte(v as u8);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(0x6, target, v_target >> 4, v_target));
-            emu.step(build_inst(0x8, target, i, 5));
-            assert_eq!(
-                emu.get_register_value(target as u8),
-                0xFF - (v - v_target) as u8
-            );
+        stress_test(|emu, x, y, val_x, val_y| {
+            emu.step(build_inst(0x8, y, x, 5));
+            assert_eq!(emu.get_register_value(y), val_y.wrapping_sub(val_x));
             assert_eq!(emu.get_register_value(0xF), 1);
-        }
+        })
     }
     #[test]
     fn test_shr() {
-        let mut emu = Chip8::new();
-        for i in 0..15 {
-            let v = rand_byte(0xFF);
-            emu.step(build_inst(0x6, i, v >> 4, v));
-            emu.step(build_inst(8, i, i, 6));
-            assert_eq!(emu.get_register_value(i as u8), (v >> 1) as u8);
-            assert_eq!(emu.get_register_value(0xF), (v & 0x01) as u8);
+        stress_test(|emu, vx, vy, val_x, val_y| {
+            emu.step(build_inst(8, vx, vy, 6));
+            assert_eq!(emu.get_register_value(vx), val_x >> 1);
+            assert_eq!(emu.get_register_value(0xF), val_x & 1);
+            // Check vy is not changed
+            assert_eq!(emu.get_register_value(vy), val_y);
+        });
+    }
+
+    /*
+     * Run a block of tests on two random registers with 2 random values assigned to them
+     * Used for basic tests
+     * Value of register X is guaranteed to be larger than value of register y
+     */
+    fn stress_test(f: fn(&mut Chip8, u8, u8, u8, u8)) {
+        let mut emu: Chip8 = Chip8::new();
+        for vx in 0..15 {
+            let mut vy = rand_byte(15 - 1) as u8;
+            if vy >= vx {
+                vy += 1;
+            }
+            let val_x = rand_byte(0xFE) as u8 + 1;
+            let val_y = rand_byte(val_x) as u8;
+            emu.step(build_inst(6, vx, val_x >> 4, val_x));
+            emu.step(build_inst(6, vy, val_y >> 4, val_y));
+            // Set VF to something other than 1 or 0
+            emu.step(build_inst(6, 0xF, 0, 2));
+            f(&mut emu, vx, vy, val_x, val_y);
         }
     }
 }
