317f31cd4eecc189fe3d7231a8ce2a476c11b33f
Josef Waller <josef@siriusapplications.com>
2024-06-08 16:50:51 -0700

    Tidy code

--- src/chip8.rs
+++ src/chip8.rs
@@ -81,52 +81,72 @@ impl Chip8 {
             println!("Initial state:");
             self.dump_state();
         }
-        // Match first digit of opcode
+        /*
+         * Match opcode
+         * Naming convention for all these functions
+         * FUNC_[ARGS...]
+         * FUNC: Description of what the function does (i.e. ld for load, add for adding)
+         * ARGS: The arguments
+         *       The first arg is the one being saved to, so ld_rx_ry will load Ry into Rx
+         *       r[x]: Register index
+         *       kk: Constant
+         *       addr: Address
+         */
         match inst & 0xF000 {
             0x0000 => match inst & 0x0FFF {
                 0x0E0 => self.clr(),
                 0x0EE => self.ret(),
                 _ => self.unknown_opcode_panic(inst),
             },
-            0x1000 => self.jmp(inst),
-            0x2000 => self.call(inst),
-            0x3000 => self.se_const(inst),
-            0x4000 => self.sne_vx_kk(inst),
-            0x5000 => self.se_reg(inst),
-            0x6000 => self.ld_vx_kk(inst),
-            0x7000 => self.add_vx_kk(inst),
+            0x1000 => self.jmp(inst & 0x0FFF),
+            0x2000 => self.call(inst & 0xFFF),
+            0x3000 => self.se_r_kk(reg_at(inst, 1), (inst & 0xFF) as u8),
+            0x4000 => self.sne_r_kk(inst),
+            0x5000 => {
+                if inst & 0xF != 0 {
+                    self.unknown_opcode_panic(inst);
+                }
+                self.se_rx_ry(reg_at(inst, 1), reg_at(inst, 2));
+            }
+            0x6000 => self.ld_r_kk(reg_at(inst, 1), (inst & 0xFF) as u8),
+            0x7000 => self.add_r_kk(reg_at(inst, 1), (inst & 0xFF) as u8),
             0x8000 => {
-                let vx = self.get_reg_idx(inst, 1);
-                let vy = self.get_reg_idx(inst, 2);
+                let rx = reg_at(inst, 1);
+                let ry = reg_at(inst, 2);
                 match inst & 0x000F {
-                    0 => self.ld_vx_vy(vx, vy),
-                    1 => self.or_vx_vy(vx, vy),
-                    2 => self.and_vx_vy(vx, vy),
-                    3 => self.xor_vx_vy(vx, vy),
-                    4 => self.add_vx_vy(vx, vy),
-                    5 => self.sub_vx_vy(vx, vy),
-                    6 => self.shr(vx),
-                    7 => self.subn(vx, vy),
-                    0xE => self.shl(vx),
+                    0 => self.ld_rx_ry(rx, ry),
+                    1 => self.or_rx_ry(rx, ry),
+                    2 => self.and_rx_ry(rx, ry),
+                    3 => self.xor_rx_ry(rx, ry),
+                    4 => self.add_rx_ry(rx, ry),
+                    5 => self.sub_rx_ry(rx, ry),
+                    6 => self.shr(rx),
+                    7 => self.subn(rx, ry),
+                    0xE => self.shl(rx),
                     _ => self.unknown_opcode_panic(inst),
                 }
             }
-            0x9000 => self.sne_vx_vy(inst),
-            0xA000 => self.ld_i(inst),
-            0xB000 => self.jmp_v0(inst),
-            0xC000 => self.rand(inst),
-            0xD000 => self.draw(inst),
+            0x9000 => {
+                if inst & 0xF != 0 {
+                    self.unknown_opcode_panic(inst);
+                }
+                self.sne_rx_ry(reg_at(inst, 1), reg_at(inst, 2));
+            }
+            0xA000 => self.ld_i(inst & 0x0FFF),
+            0xB000 => self.jmp_r0(inst & 0x0FFF),
+            0xC000 => self.rand(reg_at(inst, 1), inst & 0xFF),
+            0xD000 => self.draw(reg_at(inst, 1), reg_at(inst, 2), reg_at(inst, 3)),
             0xE000 => match inst & 0x00FF {
-                0x9E => self.skp_vx(self.get_reg_idx(inst, 1)),
-                0xA1 => self.sknp_vx(self.get_reg_idx(inst, 1)),
+                0x9E => self.skp_r(reg_at(inst, 1)),
+                0xA1 => self.sknp_r(reg_at(inst, 1)),
                 _ => self.unknown_opcode_panic(inst),
             },
             0xF000 => match inst & 0x00FF {
-                0x07 => self.ld_vx_dt(self.get_reg_idx(inst, 1)),
-                0x15 => self.ld_dt_vx(self.get_reg_idx(inst, 1)),
-                0x55 => self.store_at_i(inst),
-                0x65 => self.load_from_i(inst),
-                0x1E => self.add_i_vx(inst),
+                0x07 => self.ld_r_dt(reg_at(inst, 1)),
+                0x15 => self.ld_dt_r(reg_at(inst, 1)),
+                0x55 => self.store_at_i(reg_at(inst, 1)),
+                0x65 => self.load_from_i(reg_at(inst, 1)),
+                0x1E => self.add_i_r(reg_at(inst, 1)),
                 _ => self.unknown_opcode_panic(inst),
             },
             _ => self.unknown_opcode_panic(inst),
@@ -139,13 +159,6 @@ impl Chip8 {
 
     fn unknown_opcode_panic(&self, opcode: u16) {
         panic!("Unknown opcode '{:04X}' provided!", opcode);
-    }
-
-    // Get index of the register given the instruction
-    // and the position of the byte from the left in the instruction
-    // i.e. inst = 0xABCD, pos = 3, res = 0x000C
-    fn get_reg_idx(&self, inst: u16, pos: u8) -> usize {
-        return ((inst >> (12 - pos * 4)) & 0x000F) as usize;
     }
 
     fn dump_state(&self) {
@@ -176,59 +189,57 @@ impl Chip8 {
         println!("");
     }
 
-    fn ld_vx_kk(&mut self, inst: u16) {
-        let r = self.get_reg_idx(inst, 1);
-        let kk = (inst & 0x00FF) as u8;
+    fn ld_r_kk(&mut self, r: usize, kk: u8) {
         self.registers[r] = kk;
     }
 
-    fn ld_vx_vy(&mut self, v_x: usize, v_y: usize) {
-        self.registers[v_x] = self.registers[v_y];
+    fn ld_rx_ry(&mut self, rx: usize, ry: usize) {
+        self.registers[rx] = self.registers[ry];
     }
 
-    fn or_vx_vy(&mut self, v_x: usize, v_y: usize) {
-        self.registers[v_x] = self.registers[v_x] | self.registers[v_y];
+    fn or_rx_ry(&mut self, rx: usize, ry: usize) {
+        self.registers[rx] = self.registers[rx] | self.registers[ry];
     }
 
-    fn and_vx_vy(&mut self, v_x: usize, v_y: usize) {
-        self.registers[v_x] = self.registers[v_x] & self.registers[v_y];
+    fn and_rx_ry(&mut self, rx: usize, ry: usize) {
+        self.registers[rx] = self.registers[rx] & self.registers[ry];
     }
 
-    fn xor_vx_vy(&mut self, v_x: usize, v_y: usize) {
-        self.registers[v_x] = self.registers[v_x] ^ self.registers[v_y];
+    fn xor_rx_ry(&mut self, rx: usize, ry: usize) {
+        self.registers[rx] = self.registers[rx] ^ self.registers[ry];
     }
-    fn add_vx_vy(&mut self, vx: usize, vy: usize) {
-        let v = self.registers[vx] as u16 + self.registers[vy] as u16;
-        self.registers[vx] = v as u8;
+    fn add_rx_ry(&mut self, rx: usize, ry: usize) {
+        let v = self.registers[rx] as u16 + self.registers[ry] as u16;
+        self.registers[rx] = v as u8;
         self.registers[0xF] = if v > 0xFF { 1 } else { 0 };
     }
-    fn sub_vx_vy(&mut self, vx: usize, vy: usize) {
-        self.registers[0xF] = if self.registers[vy] > self.registers[vx] {
+    fn sub_rx_ry(&mut self, rx: usize, ry: usize) {
+        self.registers[0xF] = if self.registers[ry] > self.registers[rx] {
             1
         } else {
             0
         };
-        self.registers[vx] = self.registers[vx].wrapping_sub(self.registers[vy]);
+        self.registers[rx] = self.registers[rx].wrapping_sub(self.registers[ry]);
     }
-    fn shr(&mut self, v: usize) {
-        self.registers[0xF] = if self.registers[v] & 0x01 == 1 { 1 } else { 0 };
-        self.registers[v] = self.registers[v] >> 1;
+    fn shr(&mut self, r: usize) {
+        self.registers[0xF] = if self.registers[r] & 0x01 == 1 { 1 } else { 0 };
+        self.registers[r] = self.registers[r] >> 1;
     }
-    fn subn(&mut self, vx: usize, vy: usize) {
-        self.registers[0xF] = if self.registers[vx] > self.registers[vy] {
+    fn subn(&mut self, rx: usize, ry: usize) {
+        self.registers[0xF] = if self.registers[rx] > self.registers[ry] {
             1
         } else {
             0
         };
-        self.registers[vx] = self.registers[vy].wrapping_sub(self.registers[vx]);
+        self.registers[rx] = self.registers[ry].wrapping_sub(self.registers[rx]);
     }
-    fn shl(&mut self, v: usize) {
-        self.registers[0xF] = if self.registers[v] & 0x80 == 0x80 {
+    fn shl(&mut self, r: usize) {
+        self.registers[0xF] = if self.registers[r] & 0x80 == 0x80 {
             1
         } else {
             0
         };
-        self.registers[v] = self.registers[v] << 1;
+        self.registers[r] = self.registers[r] << 1;
     }
     fn clr(&mut self) {
         self.screen_buffer = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
@@ -237,86 +248,79 @@ impl Chip8 {
         self.sp -= 1;
         self.pc = self.stack[self.sp] as usize;
     }
-    fn jmp(&mut self, inst: u16) {
-        self.pc = ((inst & 0x0FFF) - 2) as usize;
+    fn jmp(&mut self, addr: u16) {
+        // addr - 2 since we are about to add 2
+        self.pc = (addr - 2) as usize;
     }
-    fn jmp_v0(&mut self, inst: u16) {
-        self.pc = ((inst & 0x0FFF) + self.registers[0] as u16) as usize - 2;
+    fn jmp_r0(&mut self, addr: u16) {
+        self.pc = (addr + self.registers[0] as u16) as usize - 2;
     }
-    fn call(&mut self, inst: u16) {
+    fn call(&mut self, addr: u16) {
         self.stack[self.sp] = self.pc as u16;
         self.sp += 1;
-        self.pc = (inst & 0x0FFF) as usize;
+        self.pc = addr as usize;
     }
-    fn se_const(&mut self, inst: u16) {
-        if self.registers[self.get_reg_idx(inst, 1)] as u16 == inst & 0xFF {
+    fn se_r_kk(&mut self, r: usize, kk: u8) {
+        if self.registers[r] == kk {
             self.pc += 2;
         }
     }
-    fn se_reg(&mut self, inst: u16) {
-        if self.registers[self.get_reg_idx(inst, 1)] == self.registers[self.get_reg_idx(inst, 2)] {
+    fn se_rx_ry(&mut self, rx: usize, ry: usize) {
+        if self.registers[rx] == self.registers[ry] {
             self.pc += 2;
         }
     }
-    fn sne_vx_kk(&mut self, inst: u16) {
-        if self.registers[self.get_reg_idx(inst, 1)] as u16 != inst & 0xFF {
+    fn sne_r_kk(&mut self, inst: u16) {
+        if self.registers[reg_at(inst, 1)] as u16 != inst & 0xFF {
             self.pc += 2;
         }
     }
-    fn sne_vx_vy(&mut self, inst: u16) {
-        let x = self.registers[self.get_reg_idx(inst, 1)];
-        let y = self.registers[self.get_reg_idx(inst, 2)];
-        if x != y {
+    fn sne_rx_ry(&mut self, x: usize, y: usize) {
+        if self.registers[x] != self.registers[y] {
             self.pc += 2;
         }
     }
-    fn skp_vx(&mut self, x: usize) {
+    fn skp_r(&mut self, x: usize) {
         if self.input_state[self.registers[x] as usize] {
             self.pc += 2;
         }
     }
-    fn sknp_vx(&mut self, x: usize) {
+    fn sknp_r(&mut self, x: usize) {
         if !self.input_state[self.registers[x] as usize] {
             self.pc += 2;
         }
     }
-    fn ld_i(&mut self, inst: u16) {
-        self.i = (inst & 0x0FFF) as u16;
+    fn ld_i(&mut self, addr: u16) {
+        self.i = addr;
     }
-    fn load_from_i(&mut self, inst: u16) {
-        let x = ((inst >> 8) & 0xF) + 1;
-        for j in 0..x {
-            self.registers[j as usize] = self.mem[(self.i + j) as usize];
+    fn load_from_i(&mut self, n: usize) {
+        for j in 0..(n + 1) {
+            self.registers[j as usize] = self.mem[(self.i + j as u16) as usize];
         }
     }
-    fn store_at_i(&mut self, inst: u16) {
-        let x = ((inst >> 8) & 0xF) + 1;
-        for j in 0..x {
-            self.mem[(self.i + j) as usize] = self.registers[j as usize];
+    fn store_at_i(&mut self, n: usize) {
+        for j in 0..(n + 1) {
+            self.mem[(self.i + j as u16) as usize] = self.registers[j as usize];
         }
     }
-    fn add_i_vx(&mut self, inst: u16) {
-        let x = self.get_reg_idx(inst, 1);
-        self.i = self.i.wrapping_add(self.registers[x] as u16);
+    fn add_i_r(&mut self, r: usize) {
+        self.i = self.i.wrapping_add(self.registers[r] as u16);
     }
-    fn add_vx_kk(&mut self, inst: u16) {
-        let x = self.get_reg_idx(inst, 1);
-        self.registers[x] = self.registers[x].wrapping_add((inst & 0xFF) as u8);
+    fn add_r_kk(&mut self, r: usize, kk: u8) {
+        self.registers[r] = self.registers[r].wrapping_add(kk);
     }
-    fn rand(&mut self, inst: u16) {
-        let r = rand::thread_rng().gen_range(0..0xFF) & (inst & 0xFF);
-        self.registers[self.get_reg_idx(inst, 1)] = r as u8;
+    fn rand(&mut self, r: usize, mask: u16) {
+        self.registers[r] = (rand::thread_rng().gen_range(0..0xFF) & mask) as u8;
     }
-    fn ld_dt_vx(&mut self, x: usize) {
+    fn ld_dt_r(&mut self, x: usize) {
         self.dt = self.registers[x];
     }
-    fn ld_vx_dt(&mut self, x: usize) {
+    fn ld_r_dt(&mut self, x: usize) {
         self.registers[x] = self.dt;
     }
-    fn draw(&mut self, inst: u16) {
-        let x = self.registers[self.get_reg_idx(inst, 1)];
-        let y = self.registers[self.get_reg_idx(inst, 2)];
-        let n = self.get_reg_idx(inst, 3);
+    fn draw(&mut self, rx: usize, ry: usize, n: usize) {
+        let x = self.registers[rx];
+        let y = self.registers[ry];
         // XOR data onto screen
         for j in 0..(n + 1) {
             let val = self.mem[self.i as usize + j];
@@ -351,4 +355,11 @@ impl Chip8 {
     pub fn get_dt(&self) -> u8 {
         return self.dt;
     }
+}
+
+// Get index of the register given the instruction
+// and the position of the byte from the left in the instruction
+// i.e. inst = 0xABCD, pos = 3, res = 0x000C
+fn reg_at(inst: u16, pos: u8) -> usize {
+    return ((inst >> (12 - pos * 4)) & 0x000F) as usize;
 }
